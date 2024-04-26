be0f185964a81431480bd71553a0b00e494b903a
slentsy <smdlentsy@student.ciputra.ac.id>
2024-04-26 09:16:42 +0700

    Section 2 Composing complex interfaces

--- Landmarks/CategoryHome.swift
+++ Landmarks/CategoryHome.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    
     var body: some View {
         NavigationSplitView {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .navigationTitle("Featured")
+            List{
+                ForEach(modelData.categories.keys.sorted(), id: \.self){
+                    key in Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         } detail: {
             Text("Select a Landmark")
         }
@@ -20,4 +27,5 @@ struct CategoryHome: View {
 
 #Preview {
     CategoryHome()
+        .environment(ModelData())
 }
