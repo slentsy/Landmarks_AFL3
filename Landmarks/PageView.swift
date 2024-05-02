982d5e23b610243057e32edadfe02dc22c1000d8
slentsy <smdlentsy@student.ciputra.ac.id>
2024-05-02 13:03:41 +0700

    Section 4 - Interfacing with UIKit

--- Landmarks/PageView.swift
+++ Landmarks/PageView.swift
@@ -14,9 +14,11 @@ struct PageView<Page: View>: View {
 
 
     var body: some View {
-        VStack{
+        ZStack(alignment: .bottomTrailing){
             PageViewController(pages: pages, currentPage: $currentPage)
-            Text("Current Page : \(currentPage)")
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage )
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
         }
             .aspectRatio(3 / 2, contentMode: .fit)
     }
