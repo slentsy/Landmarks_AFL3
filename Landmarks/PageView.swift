083f0c1949e3b278d8c1f59eb46b55ce53f3261e
slentsy <smdlentsy@student.ciputra.ac.id>
2024-05-02 12:46:01 +0700

    Section 2 dan section 3 - Interfacing with UIKit

--- Landmarks/PageView.swift
+++ Landmarks/PageView.swift
@@ -7,15 +7,22 @@
 
 import SwiftUI
 
+
 struct PageView<Page: View>: View {
     var pages: [Page]
-    
+    @State private var currentPage = 0
+
+
     var body: some View {
-        PageViewController(pages: pages)
+        VStack{
+            PageViewController(pages: pages, currentPage: $currentPage)
+            Text("Current Page : \(currentPage)")
+        }
             .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
 
+
 #Preview {
-    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0)})
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
 }
