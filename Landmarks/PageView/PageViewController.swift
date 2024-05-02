c2152cdaf011eaea5f199164dbc8c4806fd847e7
Sophia <smadlentsy@student.ciputra.ac.id>
2024-05-02 09:17:36 +0700

    Section 1 - Interfacing With UIKit

---
+++ Landmarks/PageView/PageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Sophia Madlentsy Tambunan on 5/2/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        
+
+
+        return pageViewController
+    }
+
+
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
+
