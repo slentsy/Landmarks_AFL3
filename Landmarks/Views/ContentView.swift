7d0a070a8ceb90ab28329c7c2a0f0977d80fc02b
Sophia <smadlentsy@student.ciputra.ac.id>
2024-05-03 08:36:59 +0700

    Section 5 - creating a watchOS app

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -6,30 +6,17 @@
 //
 //SECTION 1
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
-    @State private var selection: Tab = .featured
-    
-    enum Tab{
-        case featured
-        case list
-    }
-    
     var body: some View {
-        TabView(selection: $selection){
-            CategoryHome()
-                .tabItem {
-                    Label("Featured", systemImage: "star")
-                }
-                .tag(Tab.featured)
-            
-            LandmarksList()
-                .tabItem {
-                    Label("List", systemImage: "list.bullet")
-                }
-                .tag(Tab.list)
-            
-        }
+        LandmarksList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
     }
 }
 
--- WatchLandmarks Watch App/ContentView.swift
+++ WatchLandmarks Watch App/ContentView.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
             LandmarksList()
-        }
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
     }
+}
 
 #Preview {
     ContentView()
