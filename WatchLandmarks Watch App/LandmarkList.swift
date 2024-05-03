2ec1a741a0a4822ac35a6b7e79a1b90b041fd544
Sophia <smadlentsy@student.ciputra.ac.id>
2024-05-03 09:25:07 +0700

    Section 3 - Creating a macOS app

--- WatchLandmarks Watch App/LandmarkList.swift
+++ WatchLandmarks Watch App/LandmarkList.swift
@@ -1,49 +1,51 @@
 //
-//  LandmarksList.swift
-//  Landmarks
+//  LandmarkList.swift
+//  WatchLandmarks Watch App
 //
-//  Created by student on 19/04/24.
+//  Created by Sophia Madlentsy Tambunan on 5/3/24.
 //
 
 import SwiftUI
 
-struct LandmarksList: View {
+
+struct LandmarkList: View {
     @Environment(ModelData.self) var modelData
     @State private var showFavoritesOnly = false
-    
-    var filteredLandmarks:[Landmark] {
+
+
+    var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-    
-    
-    
+
+
     var body: some View {
-        NavigationSplitView{
-            List{
-                Toggle(isOn: $showFavoritesOnly){
+        NavigationSplitView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
                 }
-                
-                ForEach(filteredLandmarks){
-                    landmark in NavigationLink{
+
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
                         LandmarkDetail(landmark: landmark)
-                    }label: {
+                    } label: {
                         LandmarkRow(landmark: landmark)
                     }
                 }
             }
             .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
-            .frame(minWidth: 300)
         } detail: {
             Text("Select a Landmark")
         }
     }
 }
 
+
 #Preview {
-    LandmarksList()
+    LandmarkList()
         .environment(ModelData())
 }
